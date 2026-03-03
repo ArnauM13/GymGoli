07a4c069e7af2475a3dbb9f2055b39070d9aabf1
ArnauM13 <arnaumm98@gmail.com>
2026-03-03 12:31:37 +0100

    feat: component modernization

--- src/app/components/exercise-popup/exercise-popup.actions.ts
+++
@@ -1,4 +0,0 @@
-import { createAction } from '@ngrx/store';
-
-export const openPopup = createAction('[Popup] Open Popup');
-export const closePopup = createAction('[Popup] Close Popup');
--- src/app/interfaces/exercise-popup.ts
+++
@@ -1,3 +0,0 @@
-export interface PopupState {
-    isOpen: boolean;
-}
